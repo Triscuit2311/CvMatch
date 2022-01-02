1036400ef7aa464b76a9645f30f3d75e61b9418d
triscuit2311 <dante.trisciuzzi@snhu.edu>
2022-01-01 21:05:56 -0500

    Removed unused method

--- CvWindowScanner/Utils/Extensions.cs
+++ CvWindowScanner/Utils/Extensions.cs
@@ -7,14 +7,8 @@ namespace CvWindowScanner.Utils
         public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
         {
             if (val.CompareTo(min) < 0) return min;
-            else if(val.CompareTo(max) > 0) return max;
-            else return val;
-        }
-        
-        public static int ClampInt(this int val, int min, int max)
-        {
-            if (val < min) return min;
-            return val > max ? max : val;
+            if(val.CompareTo(max) > 0) return max;
+            return val;
         }
     }
 }

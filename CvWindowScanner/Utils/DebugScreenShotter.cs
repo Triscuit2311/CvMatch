808f7cfd334f69d964c763edaf0f0b94dac9e5a1
triscuit2311 <dante.trisciuzzi@snhu.edu>
2022-01-01 21:05:31 -0500

    Added DebugScreenshotter

---
+++ CvWindowScanner/Utils/DebugScreenShotter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CvWindowScanner.Utils
+{
+
+    public static class DebugScreenShotter
+    {
+        private static int _count;
+        private static string _sessionId;
+        
+        public static void Setup()
+        {
+            _sessionId = $"[{DateTime.Now.ToShortDateString().Replace('/','.')} @ {DateTime.Now.ToShortTimeString().Replace(':','.')}]";
+            HotKeyManager.RegisterHotKey(Keys.Insert, KeyModifiers.NoRepeat);
+            HotKeyManager.HotKeyPressed += SaveLastScreen;
+        }
+
+
+        private static void SaveLastScreen(object sender, HotKeyEventArgs hotKeyEventArgs)
+        {
+            if (CvSearch.LastFrame.Empty())
+            {
+                Console.WriteLine("No image to save.");
+                return;
+            }
+
+            var file = $"screenshots\\{_sessionId}_{_count}.bmp";
+
+            _count++;
+            if (CvSearch.LastFrame.SaveImage(file))
+            {
+                Console.WriteLine(file);
+                return;
+            }
+            Console.WriteLine(file);
+        }
+
+
+    }
+}

332ed56024c04426ecc6aec4e9070b9e56235d79
triscuit2311 <dante.trisciuzzi@snhu.edu>
2022-01-05 14:01:44 -0500

    bot base handler

---
+++ CvWindowScanner/Modules/BotBase.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace CvWindowScanner.Modules
+{
+    public static class BotBase
+    {
+        public static List<GameState> GameStates;
+        private static GameState _lastState = default;
+        private static GameState _currentState = default;
+        private static int _gameCycles = 0;
+
+        public static void Run(List<GameState> gameStates, int cycleSleep = 100, int windowRefreshCycles = 1000 )
+        {
+            GameStates = gameStates;
+            
+            while (!WindowScanner.Initialized)
+            {
+                Thread.Sleep(10); 
+            }
+            
+            while (WindowScanner.Initialized)
+            {
+                _gameCycles++;
+                if (_gameCycles % windowRefreshCycles == 0) WindowScanner.UpdateWindow();
+                Thread.Sleep(cycleSleep);
+                var flag = false;
+
+                var priority = 0;
+                foreach (var state in GameStates.Where(state => state.State))
+                {
+                    if (priority >= state.Priority) continue;
+                    _currentState = state;
+                    flag = true;
+                }
+                
+                BotAction.Cycle(_currentState, _gameCycles, flag);
+                
+                if (!flag || _currentState == _lastState) continue;
+                _lastState = _currentState;
+
+                Console.WriteLine($"[{_gameCycles}] State: {_currentState.Name}");
+                
+            }
+            
+        }
+
+        
+    }
+}

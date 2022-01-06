483a446870c6ccdda272517d6cc67521746109af
triscuit2311 <dante.trisciuzzi@snhu.edu>
2022-01-06 10:20:16 -0500

    ActionCallbacks

---
+++ CvWindowScanner/GameVariables/TarkovActions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CvWindowScanner.Modules;
+
+namespace CvWindowScanner.GameVariables
+{
+    public static class TarkovActions
+    {
+        public static void StopCycle(GameState currentState,
+            int currentCycle, bool isNewState)
+        {
+
+            
+        }
+        
+        public static void AsyncCycle(GameState currentState,
+            int currentCycle, bool isNewState)
+        {
+            
+        }
+        
+    }
+}

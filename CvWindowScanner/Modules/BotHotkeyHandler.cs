4def262eb3a51c05c647a716d5b2493d411a8c17
triscuit2311 <dante.trisciuzzi@snhu.edu>
2022-01-06 16:41:30 -0500

    New Hotkeys

---
+++ CvWindowScanner/Modules/BotHotkeyHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Forms;
+using CvWindowScanner.Utils;
+
+namespace CvWindowScanner.Modules
+{
+    public static class BotHotkeyHandler
+    {
+
+        public static void Setup()
+        {
+                       HotKeyManager.RegisterHotKey(Keys.End, KeyModifiers.NoRepeat);
+            HotKeyManager.HotKeyPressed += HotkeyHandler;
+        }
+        
+
+        public static void HotkeyHandler(object sender, HotKeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Keys.End:
+                    BotAction.Paused = !BotAction.Paused;
+                    Console.WriteLine($"Bot Actions are now {(BotAction.Paused? "Paused":"Resumed")}.");
+                    break;
+
+            }
+
+        }
+        
+        
+      
+    }
+}
---
+++ CvWindowScanner/Utils/DeveloperHotkeys.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Windows.Forms;
+using CvWindowScanner.Modules;
+
+namespace CvWindowScanner.Utils
+{
+    public class DeveloperHotkeys
+    {
+        private static int _count;
+        private static string _sessionId;
+
+        public static void Setup()
+        {
+            _sessionId = $"[{DateTime.Now.ToShortDateString().Replace('/','.')}" +
+                         $" @ {DateTime.Now.ToShortTimeString().Replace(':','.')}]";
+            HotKeyManager.RegisterHotKey(Keys.Insert, KeyModifiers.NoRepeat);
+            HotKeyManager.RegisterHotKey(Keys.Home, KeyModifiers.NoRepeat);
+            HotKeyManager.HotKeyPressed += HotkeyHandler;
+
+        }
+        
+        public static void HotkeyHandler(object sender, HotKeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Keys.Insert:
+                    SaveLastScreen();
+                    break;
+                case Keys.Home:
+                    PrintCurPos();
+                    break;
+
+            }
+
+        }
+        
+        public static void PrintCurPos()
+        {
+            var pt = InputWrapper.GetCursorPosition();
+            Console.WriteLine($"Cursor Position:\n" +
+                              $"\tScreen Space: ({pt.X}, {pt.Y})"+
+                              $"\tWindow Space: ({pt.X-WindowScanner.WindowPosition.X}, {pt.Y-WindowScanner.WindowPosition.Y})"
+            );
+        }
+
+        public static void SaveLastScreen()
+        {
+            if (CvSearch.LastFrame.Empty())
+            {
+                Console.WriteLine("No image to save.");
+                return;
+            }
+
+            var file = $"screenshots\\{_sessionId}_{_count}.bmp";
+
+            _count++;
+            if (CvSearch.LastFrame.SaveImage(file))
+            {
+                Console.WriteLine(file);
+                return;
+            }
+            Console.WriteLine(file);
+        }
+
+    }
+}
